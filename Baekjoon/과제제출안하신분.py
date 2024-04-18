d193cc313b6df4526b8e96b409d95104e6617fce
yunjonghyeoks <yunj05763@gmail.com>
2024-04-18 09:34:21 +0900

    5597 list 범위 수정

--- Baekjoon/과제제출안하신분.py
+++ Baekjoon/과제제출안하신분.py
@@ -2,8 +2,8 @@
 # 이때 고정으로 28명은 과제 제출했고 2명은 안함 <- 2명을 찾는거임
 
 
-stdn = set([int(i) for i in range(1, 6)]) # 총 학생 번호
-sub_stdn = set([int(input()) for _ in range(3)]) # 제출한 학생 번호
+stdn = set([int(i) for i in range(1, 31)]) # 총 학생 번호
+sub_stdn = set([int(input()) for _ in range(28)]) # 제출한 학생 번호
  
 sub = stdn & sub_stdn # 교집합 구하기
 
