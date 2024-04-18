6a4df8ce135eabd93540e6ea4349b5e0f2ba758b
yunjonghyeoks <yunj05763@gmail.com>
2024-04-18 09:33:07 +0900

    5597 문제풀이 (집합자료형 공부)

---
+++ Baekjoon/과제제출안하신분.py
@@ -0,0 +1,17 @@
+# 1 ~ 30까지 출석번호 있음
+# 이때 고정으로 28명은 과제 제출했고 2명은 안함 <- 2명을 찾는거임
+
+
+stdn = set([int(i) for i in range(1, 6)]) # 총 학생 번호
+sub_stdn = set([int(input()) for _ in range(3)]) # 제출한 학생 번호
+ 
+sub = stdn & sub_stdn # 교집합 구하기
+
+stdn -= sub # 총 학생 번호에서 공통된 학생 번호 빼기
+
+print(*stdn, sep="\n")
+
+# 1 3 4 5 6 7 8 10
+
+# 답 : 2 9
+
